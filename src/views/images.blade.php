edf7ab892488e05a94517e81141fbfd7669b7545
Haruto0307 <skybolt0307@gmail.com>
2015-07-30 07:43:09 +0000

    click thumb to use file

--- src/views/images.blade.php
+++ src/views/images.blade.php
@@ -5,7 +5,7 @@
 
             @foreach($directories as $key => $dir)
                 <div class="col-sm-6 col-md-2">
-                    <div class="thumbnail text-center" data-id="{{ basename($dir) }}">
+                    <div class="thumbnail text-center";" data-id="{{ basename($dir) }}">
                         <a id="large_folder_{{ $key }}" data-id="{{ $dir }}"
                            onclick="clickFolder('large_folder_{{ $key }}',1)"
                            class="folder-icon pointer">
@@ -41,7 +41,7 @@
                     <div class="thumbnail thumbnail-img" data-id="{{ basename($file) }}" id="img_thumbnail_{{ $key }}">
                         <img id="{!! $file !!}"
                              src="{{ $dir_location }}{{ $base }}/thumbs/{{ basename($file) }}?r={{ str_random(40) }}"
-                             alt="">
+                             alt="" style="cursor:pointer;" onclick="useFile('{!! basename($file) !!}')">
                     </div>
 
                     <div class="caption text-center">
